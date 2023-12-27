ed0371e23c4770ac9005498e81fde953f7c251f7
Benjamin Eklund <101027885+BenEklundCS@users.noreply.github.com>
2023-12-27 08:45:45 -0800

    Create IO.java

---
+++ IO.java
@@ -0,0 +1,73 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IO {
+    private Scanner scanner = new Scanner(System.in);
+
+    // Singleton design pattern (also ensures I only have one Scanner instance)
+
+    private static IO instance = null;
+
+    public static synchronized IO getInstance()
+    {
+        if (instance == null)
+            instance = new IO();
+ 
+        return instance;
+    }
+
+    public String getInput() {
+        return scanner.nextLine();
+    }
+
+    public String getValidatedInput(ArrayList<String> validStrings) {
+        String prompt = "Please enter input in this list of options: " + validStrings;
+        String result;
+        do {
+            System.out.println(prompt);
+            result = getInput();
+            prompt = "Invalid option, please try again: ";
+
+        } while (!validStrings.contains(result));
+        return result;
+    }
+
+    // File handling
+
+    public void listFiles(String path) {
+        File[] files = getFileList(path);
+        printFileList(files);
+    }
+
+    public File[] getFileList(String path) {
+        File directory = new File(path);
+        File[] result = directory.listFiles();
+        return result;
+    }
+
+    public void printFileList(File[] files) {
+        for (File f : files) {
+            System.out.println(f);
+        }
+    }
+
+    public void deleteFilesAtDir(String path) {
+
+        File[] files = getFileList(path);
+
+        for (File f : files) {
+            f.delete();
+        }
+    }
+
+    // Pathing
+
+    public String getDesktopPath() {
+        return System.getProperty("user.home") + "/Desktop";
+    }
+
+    public String getDownloadsPath() {
+        return System.getProperty("user.home") + "/Downloads";
+    }
+}
